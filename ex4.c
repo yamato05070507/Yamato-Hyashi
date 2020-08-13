d7393d6f7e16911c894526cc727dae39b4f72e15
yamato05070507 <69616599+yamato05070507@users.noreply.github.com>
2020-08-13 14:15:31 +0900

    最初のバージョン

---
+++ ex4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int i, num, heads=0,trails=0;
+
+  printf("Tossing a coin...\n");
+
+  for (i=0;i<3;i++){
+    num = rand();
+    if(num%2==0){
+      printf("Round %d: Head\n", i+1);
+      heads++;
+    }
+    else{
+      printf("Round %d: Tral\n", i+1);
+      trails++;
+    }
+  }
+  printf("Heads: %d, Trails: %d\n", heads,trails);
+  return 0;
+}
